56eb071aaad5700659f5b41ec912d209291b60c9
lmy <lmysoar@hotmail.com>
2015-03-28 16:51:43 +0800

    it can start and stop gracefully

---
+++ connecter.js
@@ -0,0 +1,88 @@
+/**
+ * Created by lmy on 15-3-28.
+ */
+
+
+var daemon = require('daemon');
+var commander = require('commander');
+var fs = require('fs');
+var PIDFILE = '/var/run/judge_controller.pid';
+var child_process = require('child_process');
+
+function stop() {
+    if (process.getgid() === 0) {
+
+    } else {
+        console.log('Please run me as root');
+        return ;
+    }
+    fs.readFile(PIDFILE, 'utf-8', function (err, PID) {
+        if(err) {
+            console.log('Can not stop it! It may be not running.');
+            console.log(err.message);
+            return;
+        }
+        console.log('The pid is' + PID);
+        child_process.spawn('kill',['-TERM',PID]).on('exit', function () {
+            console.log('killed');
+        });
+    });
+}
+
+function start() {
+    if (process.getgid() === 0) {
+
+    } else {
+        console.log('Please run me as root');
+        return ;
+    }
+    console.log('starting');
+    if(commander.no_daemon) {
+        child_process.spawn('node',['./judge_controller.js'],{stdio:'inherit'});
+    } else {
+        daemon.daemon('./judge_controller.js', []);
+    }
+}
+
+function restart() {
+    if (process.getgid() === 0) {
+
+    } else {
+        console.log('Please run me as root');
+        return ;
+    }
+    fs.readFile(PIDFILE, 'utf-8', function (err, PID) {
+        if(err) {
+            console.log('Can not restart it! It may be not running.');
+            console.log(err.message);
+            return;
+        }
+        console.log(PID);
+        child_process.spawn('kill',['-HUP',PID]).on('exit', function () {
+            console.log('restarted');
+        });
+    });
+}
+
+commander
+    .version('1.0.0')
+    .usage('[command] [options]');
+
+commander
+    .command('start')
+    .description('program will start')
+    .action(start);
+
+commander
+    .command('stop')
+    .description('program will stop')
+    .action(stop);
+
+commander
+    .command('restart')
+    .description('program will restart')
+    .action(restart);
+
+commander
+    .option('-n --no_daemon', 'if it will run as daemon')
+    .parse(process.argv);
---
+++ judge_controller.js
@@ -0,0 +1,105 @@
+/**
+ * Created by lmy on 15-3-26.
+ */
+
+
+var judge_client = require('./judge_client');
+var config = require('./config');
+var cluster = require('cluster');
+var fs = require('fs');
+var PIDFILE = '/var/run/judge_controller.pid';
+
+/**
+ * create one worker form config
+ * @param config
+ */
+function createWorker(config) {
+    // Fork a worker if running as cluster master
+    var child = cluster.fork();
+
+    // Respawn the child process after exit but the child won't be deleted and the number of it will increase...
+    // (ex. in case of an uncaught exception)
+    child.send(config);
+    child.on('exit', function (code) {
+        console.log(child.id + 'exited' + 'with code:'+code);
+        createWorker(config);
+        console.log('restart with'+config.name);
+    });
+}
+
+/**
+ * create all workers form configs
+ * @param configs
+ */
+function createWorkers(configs) {
+    configs.forEach(createWorker);
+}
+
+/**
+ * kill all workers with the given signal.
+ * also removes all event listeners form workers before sending the signal
+ * to prevent spawning again
+ * @param signal
+ */
+function killAllWorkers(signal) {
+    var uniqueID,
+        worker;
+
+    for (uniqueID in cluster.workers) {
+        if (cluster.workers.hasOwnProperty(uniqueID)) {
+            worker = cluster.workers[uniqueID];
+            worker.removeAllListeners();
+            worker.process.kill(signal);
+        }
+    }
+}
+
+
+if(cluster.isMaster) {
+    /**
+     * must run as root
+     * first step: write process.pid to PIDFILE
+     * second step: create workers
+     */
+    if (process.getgid() === 0) {
+
+    } else {
+        console.log('Please run me as root');
+        return ;
+    }
+    /**
+    * Restart the workers
+    */
+    process.on('SIGNUP', function () {
+        killAllWorkers('SIGTERM');
+        createWorkers(config);
+    });
+
+    /**
+    * Gracefully Shuts down the workers
+    */
+    process.on('SIGTERM', function () {
+        killAllWorkers('SIGTERM');
+        fs.unlink(PIDFILE, function (err) {
+            if(err) {
+                console.log(err.message);
+                return ;
+            }
+            console.log('Stopped running');
+        });
+    });
+
+    fs.writeFileSync(PIDFILE, process.pid);
+    createWorkers(config);
+
+} else {
+    /**
+     * worker will get its config
+     */
+    process.on('message', function (config) {
+        console.log('created a worker with ', config);
+        var judge = new judge_client(config);
+        judge.start();
+    });
+}
+
