6c1ae23dec4e022c67679d6be72d637a74b9087d
梁明阳 <lmysoar@hotmail.com>
2016-12-09 17:20:18 +0800

    取消了restart功能

--- connecter.js
+++ connecter.js
@@ -43,25 +43,19 @@ function start() {
     }
 }
 
-function restart() {
-    if (process.getgid() === 0) {
-
-    } else {
-        console.log('Please run me as root');
-        return ;
-    }
-    fs.readFile(PIDFILE, 'utf-8', function (err, PID) {
-        if(err) {
-            console.log('Can not restart it! It may be not running.');
-            console.log(err.message);
-            return;
-        }
-        console.log(PID);
-        child_process.spawn('kill',['-HUP',PID]).on('exit', function () {
-            console.log('restarted');
-        });
-    });
-}
+// function restart() {
+//     fs.readFile(PIDFILE, 'utf-8', function (err, PID) {
+//         if(err) {
+//             console.log('Can not restart it! It may be not running.');
+//             console.log(err.message);
+//             return;
+//         }
+//         console.log(PID);
+//         child_process.spawn('kill',['-HUP',PID]).on('exit', function () {
+//             console.log('restarted');
+//         });
+//     });
+// }
 
 commander
     .version('1.0.0')
@@ -77,10 +71,6 @@ commander
     .description('program will stop')
     .action(stop);
 
-commander
-    .command('restart')
-    .description('program will restart')
-    .action(restart);
 
 commander
     .option('-n --no_daemon', 'if it will run as daemon')
